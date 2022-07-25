ed1b93c0b6a8139ab842f019d19e39567356c12a
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2022-07-25 19:57:30 +0430

    add prototype DP

--- src/builder.ts
+++ src/builder.ts
@@ -65,23 +65,23 @@ class Director {
 }
 
 
-// function clientCode(director: Director) {
-//     const builder = new ConcreteBuilder1();
-//     director.setBuilder(builder);
+function clientCode(director: Director) {
+    const builder = new ConcreteBuilder1();
+    director.setBuilder(builder);
 
-//     console.log('Standard basic product!:');
-//     director.buildMinimalViableProduct();
-//     builder.getProduct().listParts();
+    console.log('Standard basic product!:');
+    director.buildMinimalViableProduct();
+    builder.getProduct().listParts();
 
-//     console.log('Standard full featured product!:');
-//     director.buildFullFeaturedProduct();
-//     builder.getProduct().listParts();
+    console.log('Standard full featured product!:');
+    director.buildFullFeaturedProduct();
+    builder.getProduct().listParts();
 
-//     console.log('Custom product!:');
-//     builder.producePartA();
-//     builder.producePartC();
-//     builder.getProduct().listParts();
-// }
+    console.log('Custom product!:');
+    builder.producePartA();
+    builder.producePartC();
+    builder.getProduct().listParts();
+}
 
 const director = new Director();
-// clientCode(director);
+clientCode(director);
